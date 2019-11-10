d81f272862a93dffa8093727669710e5126db3dd
Masashi <t15519ms@gmail.com>
2019-11-10 11:19:24 +0900

    Modified base_controller

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -2,7 +2,7 @@ class BaseController < ApplicationController
   require 'wikipedia'
   def home
     if params[:keyword]
-      @wiki = Wikipedia.find(params[:keyword)
+      @wiki = Wikipedia.find(params[:keyword])
     else
       @wiki= Wikipedia.find(".")
     end
